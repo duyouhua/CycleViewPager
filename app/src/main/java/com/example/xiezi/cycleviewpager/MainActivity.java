ccdfae164a284db104fb9e978cc498218aef9c66
xiezilailai <1945926686@qq.com>
2015-11-28 13:56:31 +0800

    view间动画添加及动画持续时间可修改

--- app/src/main/java/com/example/xiezi/cycleviewpager/MainActivity.java
+++ app/src/main/java/com/example/xiezi/cycleviewpager/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,10 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        try {
+            Field mScroller=null;
+            mScroller=ViewPager.class.getDeclaredField("mScroller");//获得viewpager里面的成员mScroller
+            mScroller.setAccessible(true);//原来是private成员，要将其设置为可修改
+            ViewPagerScroller scroller=new ViewPagerScroller(this);
+            mScroller.set(viewPager,scroller);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+
         LayoutInflater inflater = LayoutInflater.from(this);
         View view1 = inflater.inflate(R.layout.item, null);
-        View view2 = inflater.inflate(R.layout.item, null);
-        View view3 = inflater.inflate(R.layout.item, null);
+        View view2 = inflater.inflate(R.layout.item2, null);
+        View view3 = inflater.inflate(R.layout.item3, null);
 
         ArrayList<View> views = new ArrayList<>();
         views.add(view1);
@@ -31,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         views.add(view3);
 
         viewPager.setAdapter(new ImageAdapter(views));
+        viewPager.setPageTransformer(true, new Transform1());
 
         viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
---
+++ app/src/main/java/com/example/xiezi/cycleviewpager/ViewPagerScroller.java
@@ -0,0 +1,34 @@
+package com.example.xiezi.cycleviewpager;
+
+import android.content.Context;
+import android.view.animation.Interpolator;
+import android.widget.Scroller;
+
+/**
+ * Created by 蝎子莱莱123 on 2015/11/28.
+ */
+public class ViewPagerScroller extends Scroller {
+    private int mDuration=3000;
+    public ViewPagerScroller(Context context) {
+        super(context);
+    }
+
+    public ViewPagerScroller(Context context, Interpolator interpolator) {
+        super(context, interpolator);
+    }
+
+    public ViewPagerScroller(Context context, Interpolator interpolator, boolean flywheel) {
+        super(context, interpolator, flywheel);
+    }
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy) {
+        super.startScroll(startX, startY, dx, dy,mDuration);
+    }
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+        super.startScroll(startX, startY, dx, dy, mDuration);
+    }
+
+}
